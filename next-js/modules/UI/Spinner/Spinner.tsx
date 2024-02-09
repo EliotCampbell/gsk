9dd720577ff16522a90eb39bbd8cd970a870dd29
Georgii Dmitruk <118625048+EliotCampbell@users.noreply.github.com>
2024-02-09 21:34:10 +0400

    Create UI Spinner

---
+++ next-js/modules/UI/Spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './Spinner.module.css'
+import { FiRotateCw } from 'react-icons/fi'
+
+const Spinner: React.FC<{ rotate?: boolean }> = ({ rotate = true }) => {
+  return (
+    <FiRotateCw
+      className={
+        rotate
+          ? `${classes.spinner}, ${classes.spinner_pending}`
+          : classes.spinner
+      }
+    />
+  )
+}
+
+export default Spinner
--- next-js/modules/auth/components/SubmitButton/SubmitButton.tsx
+++ next-js/modules/auth/components/SubmitButton/SubmitButton.tsx
@@ -3,14 +3,14 @@
 import React from 'react'
 import classes from './SubmitButton.module.css'
 import { useFormStatus } from 'react-dom'
-import { FiRotateCw } from 'react-icons/fi'
+import Spinner from '@/modules/UI/Spinner/Spinner'
 
 const SubmitButton: React.FC<{ children: string }> = ({ children }) => {
   const { pending } = useFormStatus()
 
   return (
     <button className={classes.button} type="submit" aria-disabled={pending}>
-      {pending ? <FiRotateCw className={classes.spinner} /> : children}
+      {pending ? <Spinner /> : children}
     </button>
   )
 }
