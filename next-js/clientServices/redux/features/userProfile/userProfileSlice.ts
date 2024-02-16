449d5bc00a8ae82f59c680018d0b381cc275fdca
Georgii Dmitruk <118625048+EliotCampbell@users.noreply.github.com>
2024-02-16 23:24:01 +0400

    Add more logic

--- next-js/clientServices/redux/features/userProfile/userProfileSlice.ts
+++ next-js/clientServices/redux/features/userProfile/userProfileSlice.ts
@@ -1,37 +1,94 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { userSA } from '@/serverServices/supabase/exports'
+import { setError } from '@/clientServices/redux/features/notification/notificationSlice'
+import { User } from '@supabase/gotrue-js'
+import { Database, Tables } from '@/types/supabase'
 
 type TProfileState = {
-  user: {}
+  userPrivateData: {}
+  userPublicData: {}
   isLoading: boolean
 }
 
-type TThunkApi = { extra: { userSA: typeof userSA } }
+type TThunkApi = { extra: { userSA: typeof userSA } } //todo: export this
 
-const initialState: TProfileState = { isLoading: true, user: {} }
+const initialState: TProfileState = {
+  isLoading: true,
+  userPrivateData: {},
+  userPublicData: {}
+}
 
-export const getUserProfileData = createAsyncThunk<void, void, TThunkApi>(
+type TUserProfileData = {
+  user: User
+}
+
+export const getPrivateUser = createAsyncThunk<
+  TUserProfileData,
+  void,
+  TThunkApi
+>('userProfile/getUser', async (_, thunkAPI) => {
+  try {
+    const data = await thunkAPI.extra.userSA.serverGetPrivateUser()
+    if ('error' in data && data.error) {
+      throw new Error(data.error.message)
+    }
+    if ('user' in data && data.user) {
+      return data
+    } else {
+      throw new Error('Unexpected server response')
+    }
+  } catch (error) {
+    thunkAPI.dispatch(setError((error as Error).message))
+    return thunkAPI.rejectWithValue((error as Error).message)
+  }
+})
+
+export const getPublicUser = createAsyncThunk<{}, string, TThunkApi>( //todo: add type
   'userProfile/getUser',
-  async (_, thunkAPI) => {
-    const response = await thunkAPI.extra.userSA.serverGetUserProfileData()
-    console.log(response)
+  async (userId, thunkAPI) => {
+    try {
+      const data = await thunkAPI.extra.userSA.serverGetPublicUser(userId)
+      if ('error' in data && data.error) {
+        throw new Error(data.error.message)
+      }
+      if ('data' in data && data) {
+        return data
+      } else {
+        throw new Error('Unexpected server response')
+      }
+    } catch (error) {
+      thunkAPI.dispatch(setError((error as Error).message))
+      return thunkAPI.rejectWithValue((error as Error).message)
+    }
   }
 )
-
+//todo:combine functions
 export const userProfileSlice = createSlice({
   name: 'userProfile',
   initialState,
   reducers: {
-    setUserProfile: (_, action) => {
-      return { ...action.payload }
+    setUserProfile: (_, action: PayloadAction<{ str: string }>) => {
+      //todo: должно совпадать с ретюрном санка
+      return { isLoading: false, userPrivateData: {}, userPublicData: {} }
     }
   },
   extraReducers: (builder) => {
-    builder.addCase(getUserProfileData.pending, (state) => {
+    builder.addCase(getPrivateUser.pending, (state) => {
       return { ...state, isLoading: true }
     })
-    builder.addCase(getUserProfileData.fulfilled, () => {})
-    builder.addCase(getUserProfileData.rejected, () => {})
+    builder.addCase(
+      getPrivateUser.fulfilled,
+      (state, action: PayloadAction<TUserProfileData>) => {
+        return {
+          ...state,
+          isLoading: false,
+          userPrivateData: action.payload.user
+        }
+      }
+    )
+    builder.addCase(getPrivateUser.rejected, () => {
+      return { ...initialState, isLoading: false }
+    })
   }
 })
 
