282efb67413dd013b561d9714a3f3fabd124b0d0
Georgii Dmitruk <118625048+EliotCampbell@users.noreply.github.com>
2024-02-16 23:22:59 +0400

    Split different functions

--- next-js/serverServices/supabase/serverActions/userActions.ts
+++ next-js/serverServices/supabase/serverActions/userActions.ts
@@ -1,18 +1,33 @@
 'use server'
 import { createSupabaseSAClient as supabase } from '@/serverServices/supabase/clientCreators'
 
-export const serverGetUserProfileData = async () => {
+export const serverGetPrivateUser = async () => {
   try {
     const { data } = await supabase().auth.getUser()
-    if (data.user?.id) {
-      const info = await supabase()
-        .from('user_profiles')
-        .select()
-        .eq('id', data.user.id)
-      console.log(info)
+    if (data.user) {
+      return data
     }
-    return data
+    throw new Error('Unexpected supabase response')
   } catch (error) {
-    return { data: { error: { message: (error as Error).message } } }
+    return { error: { message: (error as Error).message } }
+  }
+}
+
+export const serverGetPublicUser = async (userId: string) => {
+  try {
+    const { error, data } = await supabase()
+      .from('users_profiles')
+      .select('id,name,surname,profile_image')
+      .eq('id', userId)
+      .single()
+    if (error) {
+      throw error
+    }
+    if (data) {
+      return data
+    }
+    throw new Error('Unexpected supabase response')
+  } catch (error) {
+    return { error: { message: (error as Error).message } }
   }
 }
