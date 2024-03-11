fa5932980e13f62685f615e97e56833028a3b454
Georgii Dmitruk <118625048+EliotCampbell@users.noreply.github.com>
2024-03-11 13:31:20 +0400

    New functionality of the Spinner component

--- next-js/modules/UI/Spinner/Spinner.tsx
+++ next-js/modules/UI/Spinner/Spinner.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import classes from './Spinner.module.css'
 import { FiRotateCw } from 'react-icons/fi'
 
-const Spinner: React.FC<{ rotate?: boolean }> = ({ rotate = true }) => {
+const Spinner: React.FC<{ rotate?: boolean; color?: 'colored' | 'white' }> = ({
+  rotate = true,
+  color = 'colored'
+}) => {
+  const styles = `${classes.spinner} ${
+    color === 'colored' && classes.spinner_colored
+  } ${color === 'white' && classes.spinner_white} ${
+    rotate && classes.spinner_pending
+  }
+      `
+
   return (
     <div className={classes.spinnerContainer}>
-      <FiRotateCw
-        className={
-          rotate
-            ? `${classes.spinner} ${classes.spinner_pending}`
-            : classes.spinner
-        }
-      />
+      <FiRotateCw className={styles} />
     </div>
   )
 }
