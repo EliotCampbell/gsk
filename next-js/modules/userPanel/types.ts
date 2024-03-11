7b6a15bb08006dea18db3dd77d2e02b03a63d3a5
Georgii Dmitruk <118625048+EliotCampbell@users.noreply.github.com>
2024-03-12 02:20:58 +0400

    Remove old types

--- next-js/modules/userPanel/types.ts
+++ next-js/modules/userPanel/types.ts
@@ -7,23 +7,3 @@ export type UserInfoType = {
     status: STATUS
   }
 }
-
-export type ObjectType = {
-  id: string
-  type: 'garage' | 'parking_space'
-  individual_number: string
-  ownerships: {
-    owner_since: Date
-    owner_until: Date | null
-  }[]
-}
-
-export type AdType = {
-  id: string
-  created_at: Date
-  title: string
-  body: string
-  is_hidden: boolean
-  user_id: string
-  ad_images: string[]
-}
