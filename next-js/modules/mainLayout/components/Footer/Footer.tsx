e21cb00ae0cca1ab8b4b142655f5c8e66a785674
Georgii Dmitruk <118625048+EliotCampbell@users.noreply.github.com>
2024-03-15 21:34:56 +0400

    Trash removal

--- next-js/modules/mainLayout/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import classes from "./Footer.module.css";
-const FooterModule = () => {
-  return <div className={classes.Footer}></div>;
-};
-
-export default FooterModule;
